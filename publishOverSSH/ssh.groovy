5d2964d424bc94e7be037725aa171a7704a59bda
jalonso <jalonso@localhost.localdomain>
2018-04-20 15:28:04 +0200

    update

--- publishOverSSH/ssh.groovy
+++ publishOverSSH/ssh.groovy
@@ -4,9 +4,8 @@ pipeline {
         stage('publish') {
             steps {
                 sh 'scp /tmp/test.test jalonso@192.168.20.100:/tmp'
-                sh 'nohup ssh jalonso@192.168.20.100'
-                sh 'cd /tmp'
-                sh 'touch prueba'
+                sh 'ssh jalonso@192.168.20.100 touch /tmp/prueba'
+                sh 'ssh jalonso@192.168.20.100 touch /tmp/prueba2'
             }
         }
 
