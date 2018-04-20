df21c15262c6821cc6334ea3a2b42cfb245825c2
jalonso <jalonso@localhost.localdomain>
2018-04-20 15:07:15 +0200

    update

--- publishOverSSH/ssh.groovy
+++ publishOverSSH/ssh.groovy
@@ -4,6 +4,9 @@ pipeline {
         stage('publish') {
             steps {
                 sh 'scp /tmp/test.test jalonso@192.168.20.100:/tmp'
+                sh 'ssh jalonso@192.168.20.100'
+                sh 'cd /tmp'
+                sh 'touch prueba'
             }
         }
 
